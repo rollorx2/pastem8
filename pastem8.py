858e22c32b13a2c286ae8fc4accb1356d46cc99f
rollorx2 <112563167+rollorx2@users.noreply.github.com>
2022-12-26 13:22:44 +1000

    Update pastem8.py

--- pastem8.py
+++ pastem8.py
@@ -13,7 +13,7 @@ if not os.path.exists(data_folder):
 emailpass_folder = 'emailpass'
 if not os.path.exists(emailpass_folder):
     os.makedirs(emailpass_folder)
-    
+
 # Set up a folder to store the upass files
 upass_folder = 'upass'
 if not os.path.exists(upass_folder):
@@ -25,33 +25,15 @@ regexes = [
     (re.compile(r'[\w\d\!\@\#\$\%\^\&\*\(\)\_\+]+:[\w\d\!\@\#\$\%\^\&\*\(\)\_\+]{8,}'), 'upass')
 ]
 
-while True:
-    # Query the API
-    response = requests.get('https://scrape.pastebin.com/api_scraping.php?limit=100')
+if __name__ == "__main__":
+    while True:
+        # Query the API
+        response = requests.get('https://scrape.pastebin.com/api_scraping.php?limit=100')
 
-    # Parse the JSON response
-    pastes = json.loads(response.text)
+        # Parse the JSON response
+        pastes = json.loads(response.text)
 
-    # Iterate over the pastes
-    for paste in pastes:
-        # Check if the paste has already been downloaded
-        if os.path.exists(os.path.join(data_folder, paste['key'] + '.txt')):
-            continue
-
-        # Download the paste data
-        paste_response = requests.get(paste['scrape_url'])
-        paste_data = paste_response.text
-
-        # Save the paste data to a file
-        with open(os.path.join(data_folder, paste['key'] + '.txt'), 'w') as f:
-            f.write(paste_data)
-
-        # Check if the paste matches any of the regular expressions
-        for regex, folder in regexes:
-            if regex.search(paste_data):
-                # Move the paste to the appropriate folder
-                os.rename(os.path.join(data_folder, paste['key'] + '.txt'), os.path.join(folder, paste['key'] + '.txt'))
-                break
-
-    # Wait one minute before querying the API again
-    time.sleep(60)
+        # Iterate over the pastes
+        for paste in pastes:
+            # Check if the paste has already been downloaded
+            if os.
